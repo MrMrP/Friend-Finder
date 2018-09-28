16b27e97210af29244fe1188a73c860c40652d5b
Marcus Price <marcusprice@MacBook-Pro-6.local>
2018-09-28 18:05:27 -0400

    functionality test

---
+++ app/data/friends.js
@@ -0,0 +1,45 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+      name: "Marcus",
+      photo: "https://media.licdn.com/dms/image/C4E03AQEzI-i1GGYiaw/profile-displayphoto-shrink_100_100/0?e=1543449600&v=beta&t=bs5VlkZB0ks-O0OyMwv04Wo734jePc8tjJemi5J1_EM",
+      scores: [
+        "5",
+        "3",
+        "3",
+        "4",
+        "5",
+        "4",
+        "2",
+        "1",
+        "3",
+        "3"
+      ]
+    },
+
+    {
+        "name":"Ahmed",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+          ]
+      }
+]
+  
+  // Here we export the array. This makes it accessible to other files using require.
+  module.exports = friends;
+  
